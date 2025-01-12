3b3d1c4582605d46c59b618a8b1ec80243393904
Moien Bhat <bhatmoien34@gmail.com>
2025-01-12 23:46:40 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,7 @@
+a = (1, 32, 345, False, "Shimran")
+print(type(a))         # tuple is  immutable
+
+
+no = a.count(345)
+print (no)
+print(a.index(345))
---
+++ 13.py
@@ -0,0 +1,10 @@
+marks = {
+    "Harris": 100,
+    "Shunron":59,
+    "Roman":51
+}               # Dictionary is mutable 
+
+print(marks["Harris"])
+print(marks.items())
+
+marks.update({"Harris": 93, "Rohan": 56})
