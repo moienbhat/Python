6aa282d8ac20082098fcbf23bd4d2bd9251b242f
Moien Bhat <bhatmoien34@gmail.com>
2025-04-21 18:04:48 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,8 @@
+name = "Smith"
+
+nameshort = name[0:3]       # start from index 0 to 3(ecxluding 3)
+
+print(nameshort)
+character1 = name[0]
+
+print(character1)
---
+++ 23.py
@@ -0,0 +1,9 @@
+name = "smith"
+
+print(name[0:3])
+
+print(name[-4:-1])     #  negative slicing 
+print(name[1:4])
+
+print(name[:4])
+print(name[1:])
